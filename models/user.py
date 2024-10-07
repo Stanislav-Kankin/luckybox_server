5714550152bcb2f07f5f885590484df4bfa4e87a
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-10-07 20:22:55 +0300

    add documentasion

--- models/user.py
+++ models/user.py
@@ -4,6 +4,14 @@ from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class User(Model):
+    """Класс User является моделью в Tortoise ORM.
+    Внутри класса определены поля id, username,
+    luckyboxes и balance. Поле id является первичным
+    ключом и не может быть null. Поле username может
+    быть null и имеет максимальную длину в 32 символа.
+    Поля luckyboxes и balance имеют значения по
+    умолчанию 1 и 0 соответственно. Эти поля используются
+    для хранения информации о пользователе в базе данных."""
     id = fields.BigIntField(pk=True)
     username = fields.CharField(max_length=32, null=True)
     luckyboxes = fields.IntField(default=1)
